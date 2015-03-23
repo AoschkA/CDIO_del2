082c4a82a6b14564f565b3df4d98232fcc67bfa7
ZeenGoone <Charles@10.16.167.214>
2015-03-23 16:11:34 +0100

    changes

--- 12_CDIO_del2/src/control/IOController.java
+++ 12_CDIO_del2/src/control/IOController.java
@@ -11,7 +11,7 @@ import exceptions.InputLengthException;
 
 public class IOController implements Runnable {
 	Thread t;
-	String name = "IO-Tr�d";
+	String name = "IO-Tråd";
 	int userdisc = 1;
 	static ServerSocket listener;
 	private static int menutrue = 0;
@@ -31,8 +31,8 @@ public class IOController implements Runnable {
 					sock.getInputStream()));
 			outstream = new DataOutputStream(sock.getOutputStream());
 			this.vaegtdata.setConnected_host(sock.getInetAddress());
-			System.out.println("Venter p� connection p� port " + portdst);
-			outstream.writeBytes("Velkommen til V�gt v.1.0 " + "\r\n");
+			System.out.println("Currently connected: " + sock.getInetAddress() + " Port: " + portdst);
+			outstream.writeBytes("Velkommen til Vægt v.1.0 " + "\r\n");
 			// outstream.writeBytes("\r" + "\n");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -49,8 +49,8 @@ public class IOController implements Runnable {
 					sock.getInputStream()));
 			outstream = new DataOutputStream(sock.getOutputStream());
 			this.vaegtdata.setConnected_host(sock.getInetAddress());
-			System.out.println("Venter p� connection p� port " + portdst);
-			outstream.writeBytes("Velkommen til Mettler BBK V�gt-simulator " + "\r\n");
+			System.out.println("Venter på connection på port " + portdst);
+			outstream.writeBytes("Velkommen til Mettler BBK Vægt-simulator " + "\r\n");
 			// outstream.writeBytes("\r" + "\n");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -68,10 +68,10 @@ public class IOController implements Runnable {
 					sock.getInputStream()));
 			outstream = new DataOutputStream(sock.getOutputStream());
 			this.vaegtdata.setConnected_host(sock.getInetAddress());
-			System.out.println("Venter p� connection p� port " + portdst);
-			outstream.writeBytes("Velkommen til Mettler BBK V�gt-simulator " + "\r\n");
+			System.out.println("Venter på connection på port " + portdst);
+			outstream.writeBytes("Velkommen til Mettler BBK Vægt-simulator " + "\r\n");
 			if (menutrue == 1){
-				outstream.writeBytes("Tryk A for at vise v�gtens kommandoer. " + "\r\n");
+				outstream.writeBytes("Tryk A for at vise vægtens kommandoer. " + "\r\n");
 			}
 			// outstream.writeBytes("\r" + "\n");
 		} catch (IOException e) {
@@ -89,23 +89,23 @@ public class IOController implements Runnable {
 		try {
 
 			while (!(inline = instream.readLine().toUpperCase()).isEmpty()) {
-
-				if (inline.startsWith("�")) {
+				System.out.println(inline);
+				if (inline.startsWith("Ÿ")) {
 					inline = inline.substring(21, inline.length());
 				}
 				if (inline.startsWith("A")) {
 					menutrue = 2;
 				}
 				if (menutrue == 2){
-					outstream.writeBytes("Kommandoer til v�gten: " + "\r\n");
-					outstream.writeBytes("1: T - Tarer v�gten " + "\r\n");
-					outstream.writeBytes("2: S - Vis nuv�rende v�gt " + "\r\n");
-					outstream.writeBytes("3: B ... - �ndre brutto v�gt p� v�gten " + "\r\n");
-					outstream.writeBytes("4: D ... - �ndre display 1's tekst " + "\r\n");
+					outstream.writeBytes("Kommandoer til vægten: " + "\r\n");
+					outstream.writeBytes("1: T - Tarer vægten " + "\r\n");
+					outstream.writeBytes("2: S - Vis nuværende vægt " + "\r\n");
+					outstream.writeBytes("3: B ... - ændre brutto vægt på vægten " + "\r\n");
+					outstream.writeBytes("4: D ... - ændre display 1's tekst " + "\r\n");
 					outstream.writeBytes("5: DW - nulstil display 1 " + "\r\n");
 					outstream.writeBytes("6: P111 - blahblah " + "\r\n");
-					outstream.writeBytes("7: RM20 8 ... - kommandoer til v�gtens bruger " + "\r\n");
-					outstream.writeBytes("8: Q - afslut v�gt " + "\r\n");
+					outstream.writeBytes("7: RM20 8 ... - kommandoer til vægtens bruger " + "\r\n");
+					outstream.writeBytes("8: Q - afslut vægt " + "\r\n");
 					menutrue = 1;
 				}
 				if (inline.startsWith("RM20")) {
@@ -160,14 +160,14 @@ public class IOController implements Runnable {
 					String temp = inline.substring(2, inline.length());
 					this.vaegtdata.setStreng_fra_bruger(inline.substring(0));
 					this.vaegtdata.setBrutto(Double.parseDouble(temp));
-					outstream.writeBytes("B " + "�ndret Brutto til: "
+					outstream.writeBytes("B " + "ændret Brutto til: "
 							+ +(vaegtdata.getBrutto()) + " kg " + "cr" + "lf"
 							+ "\r\n");
 				} else if ((inline.startsWith("Q"))) {
 					// denne ordre findes heller ikke p� den fysiske v�gt.
 					this.vaegtdata.setStreng_fra_bruger(inline.substring(0));
 					System.out.println("");
-					System.out.println("Program stoppet Q modtaget p� com port"
+					System.out.println("Program stoppet Q modtaget på com port"
 							+ "\r\n");
 					System.in.close();
 					System.out.close();
--- 12_CDIO_del2/src/control/WeightData.java
+++ 12_CDIO_del2/src/control/WeightData.java
@@ -4,20 +4,20 @@ import java.net.InetAddress;
 
 public class WeightData {
 
-	double tara = 0;
-	double brutto = 0;
-	String instruktionsdisplay1 = "";
-	String instruktionsdisplay2 = "";
-	String streng_fra_bruger = "";
+	static double tara = 0;
+	static double brutto = 0;
+	static String instruktionsdisplay1 = "";
+	static String instruktionsdisplay2 = "";
+	static String streng_fra_bruger = "";
 	InetAddress connected_host = null;
-	String rm20_kommando = "";
+	static String rm20_kommando = "";
 	
 	public String getRm20_kommando() {
 		return rm20_kommando;
 	}
 
 	public void setRm20_kommando(String rm20_kommando) {
-		this.rm20_kommando = rm20_kommando;
+		WeightData.rm20_kommando = rm20_kommando;
 	}
 
 	public InetAddress getConnected_host() {
@@ -33,7 +33,7 @@ public class WeightData {
 	}
 
 	public void setTara(double tara) {
-		this.tara = tara;
+		WeightData.tara = tara;
 	}
 
 	public double getBrutto() {
@@ -41,11 +41,11 @@ public class WeightData {
 	}
 
 	public void setBrutto(double brutto) {
-		this.brutto = brutto;
+		WeightData.brutto = brutto;
 	}
 
 	public double getNetto() {
-		return this.brutto - this.tara;
+		return WeightData.brutto - WeightData.tara;
 	}
 
 
@@ -54,7 +54,7 @@ public class WeightData {
 	}
 
 	public void setInstruktionsdisplay1(String instruktionsdisplay1) {
-		this.instruktionsdisplay1 = instruktionsdisplay1;
+		WeightData.instruktionsdisplay1 = instruktionsdisplay1;
 	}
 
 	public String getInstruktionsdisplay2() {
@@ -62,7 +62,7 @@ public class WeightData {
 	}
 
 	public void setInstruktionsdisplay2(String instruktionsdisplay2) {
-		this.instruktionsdisplay2 = instruktionsdisplay2;
+		WeightData.instruktionsdisplay2 = instruktionsdisplay2;
 	}
 
 	public String getStreng_fra_bruger() {
@@ -70,7 +70,7 @@ public class WeightData {
 	}
 
 	public void setStreng_fra_bruger(String streng_fra_bruger) {
-		this.streng_fra_bruger = streng_fra_bruger;
+		WeightData.streng_fra_bruger = streng_fra_bruger;
 	}
 	
 	public void taraWeight() {
