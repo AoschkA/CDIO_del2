33ceeb73a2d0b8c48cdb31b1501fb0fac1e4945d
ZeenGoone <Charles@10.16.167.214>
2015-03-23 10:42:24 +0100

    ændret launch og clientcontroller

--- 12_CDIO_del2/src/control/ClientController.java
+++ 12_CDIO_del2/src/control/ClientController.java
@@ -3,14 +3,6 @@ package control;
 import java.io.IOException;
 
 public class ClientController {
-	public static void main(String args[]){
 		
-		try {
-			IOController test = new IOController();
-			test.start();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+
 }
--- 12_CDIO_del2/src/run/launch.java
+++ 12_CDIO_del2/src/run/launch.java
@@ -1,10 +1,17 @@
 package run;
 
+import java.io.IOException;
+import control.IOController;
+
 public class launch {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String args[]){
+		
+		try {
+			IOController io = new IOController();
+			io.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
