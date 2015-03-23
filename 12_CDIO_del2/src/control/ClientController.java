2c599e49ce6daa78e0a57c271b90c63a943aa3fc
ZeenGoone <Charles@10.16.167.214>
2015-03-23 10:23:35 +0100

    blahblah

--- 12_CDIO_del2/src/control/ClientController.java
+++ 12_CDIO_del2/src/control/ClientController.java
@@ -1,5 +1,16 @@
 package control;
 
-public class ClientController {
+import java.io.IOException;
 
+public class ClientController {
+	public static void main(String args[]){
+		
+		try {
+			IOController test = new IOController();
+			test.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- 12_CDIO_del2/src/control/IOController.java
+++ 12_CDIO_del2/src/control/IOController.java
@@ -7,80 +7,108 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class IOController {
+public class IOController implements Runnable {
+	Thread t;
+	String name = "IO-Tråd";
 	static ServerSocket listener;
+	private static boolean running = true;
 	private static String inline;
 	private static int portdst = 8000;
 	private static Socket sock;
 	private static BufferedReader instream;
 	private static DataOutputStream outstream;
-	
-	public void user_Input(WeightData vaegtdata) throws IOException{
+	WeightData vaegtdata = new WeightData();
+
+	public IOController() throws IOException{
+		
+	}
+	public void user_Input() throws IOException {
 		listener = new ServerSocket(portdst);
-		System.out.println("Venter på connection på port " + portdst );
-		System.out.println("Indtast eventuel portnummer som 1. argument");
-		System.out.println("på kommando linien for andet portnr");
+		System.out.println("Venter på connection på port " + portdst);
+		// System.out.println("Indtast eventuel portnummer som 1. argument");
+		// System.out.println("på kommando linien for andet portnr");
 		sock = listener.accept();
-		vaegtdata.setConnected_host(sock.getInetAddress());
-		instream = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		this.vaegtdata.setConnected_host(sock.getInetAddress());
+		instream = new BufferedReader(new InputStreamReader(
+				sock.getInputStream()));
 		outstream = new DataOutputStream(sock.getOutputStream());
-		try{
-			while (!(inline = instream.readLine().toUpperCase()).isEmpty()){
-				if (inline.startsWith("RM20")){
-					vaegtdata.setStreng_fra_bruger(inline.substring(0));
-					vaegtdata.setRm20_kommando(inline.substring(2, inline.length()));
+		try {
+			while (!(inline = instream.readLine().toUpperCase()).isEmpty()) {
+				if (inline.startsWith("RM20")) {
+					this.vaegtdata.setStreng_fra_bruger(inline.substring(0));
+					this.vaegtdata.setRm20_kommando(inline.substring(2,
+							inline.length()));
 					outstream.writeBytes("RM20 " + "B");
-				}
-				else if (inline.startsWith("DW")) {
-					vaegtdata.setInstruktionsdisplay1("");
-					vaegtdata.setStreng_fra_bruger(inline.substring(0));
+				} else if (inline.startsWith("DW")) {
+					this.vaegtdata.setInstruktionsdisplay1("");
+					this.vaegtdata.setStreng_fra_bruger(inline.substring(0));
 					outstream.writeBytes("DW " + "A" + "cr" + "lf");
-				}
-				else if (inline.startsWith("D")){
+				} else if (inline.startsWith("D")) {
 					if (inline.equals("D")) {
-						vaegtdata.setInstruktionsdisplay1("");
-						vaegtdata.setStreng_fra_bruger(inline.substring(0));
+						this.vaegtdata.setInstruktionsdisplay1("");
+						this.vaegtdata.setStreng_fra_bruger(inline.substring(0));
 						outstream.writeBytes("D " + "A" + "cr" + "lf");
+					} else {
+						this.vaegtdata.setStreng_fra_bruger(inline.substring(0));
+						this.vaegtdata.setInstruktionsdisplay1(inline.substring(2,
+								inline.length()));
 					}
-					else {
-						vaegtdata.setStreng_fra_bruger(inline.substring(0));
-						vaegtdata.setInstruktionsdisplay1(inline.substring(2, inline.length()));
-					}
-				}
-				else if (inline.startsWith("P111")){
-					vaegtdata.setStreng_fra_bruger(inline.substring(0));
-					vaegtdata.setInstruktionsdisplay2(inline.substring(2, inline.length()));
+				} else if (inline.startsWith("P111")) {
+					this.vaegtdata.setStreng_fra_bruger(inline.substring(0));
+					this.vaegtdata.setInstruktionsdisplay2(inline.substring(2,
+							inline.length()));
 					outstream.writeBytes("P111 " + "A" + "cr" + "lf");
-				}
-				else if (inline.startsWith("T")){
-					vaegtdata.setStreng_fra_bruger(inline.substring(0));
-					vaegtdata.setTara(vaegtdata.getBrutto());
-					outstream.writeBytes("T " + "S " + "    " + (vaegtdata.getTara()) + " kg "+"cr" + "lf");
-				}
-				else if (inline.startsWith("S")){
-					vaegtdata.setStreng_fra_bruger(inline.substring(0));
-					outstream.writeBytes("S " + "S " + "    " + (vaegtdata.getNetto()) + " kg "+"cr" + "lf");
-				}
-				else if (inline.startsWith("B")){ 
+				} else if (inline.startsWith("T")) {
+					this.vaegtdata.setStreng_fra_bruger(inline.substring(0));
+					this.vaegtdata.setTara(vaegtdata.getBrutto());
+					outstream.writeBytes("T " + "S " + "    "
+							+ (vaegtdata.getTara()) + " kg " + "cr" + "lf");
+				} else if (inline.equals("S")) {
+					this.vaegtdata.setStreng_fra_bruger(inline.substring(0));
+					outstream.writeBytes("S " + "S " + "    "
+							+ (vaegtdata.getNetto()) + " kg " + "cr" + "lf");
+				} else if (inline.startsWith("B")) {
 					// denne ordre findes
-					//ikke på en fysisk vægt
-					String temp= inline.substring(2,inline.length());
-					vaegtdata.setStreng_fra_bruger(inline.substring(0));
-					vaegtdata.setBrutto(Double.parseDouble(temp));
-				}
-				else if ((inline.startsWith("Q"))){
-					vaegtdata.setStreng_fra_bruger(inline.substring(0));
+					// ikke på en fysisk vægt
+					String temp = inline.substring(2, inline.length());
+					this.vaegtdata.setStreng_fra_bruger(inline.substring(0));
+					this.vaegtdata.setBrutto(Double.parseDouble(temp));
+				} else if ((inline.startsWith("Q"))) {
+					// denne ordre findes heller ikke på den fysiske vægt.
+					this.vaegtdata.setStreng_fra_bruger(inline.substring(0));
 					System.out.println("");
-					System.out.println("Program stoppet Q modtaget paa com port");
+					System.out
+							.println("Program stoppet Q modtaget på com port");
 					System.in.close();
 					System.out.close();
 					instream.close();
 					outstream.close();
 				}
 			}
+		} catch (Exception e) {
+			System.out.println("Exception her: " + e.getMessage());
 		}
-		catch (Exception e){
-			System.out.println("Exception: "+e.getMessage());
+	}
+
+	@Override
+	public void run() {
+
+		while (running) {
+			try {
+				user_Input();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public void start() {
+		System.out.println("Starting " + name);
+		if (t == null) {
+			t = new Thread(this, name);
+			t.start();
 		}
 	}
 }
