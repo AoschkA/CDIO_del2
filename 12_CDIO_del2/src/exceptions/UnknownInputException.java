6cde0179af0e329cc625f49d6b9fa417f6133093
AoschkA <jonaspraem@gmail.com>
2015-03-23 15:25:00 +0100

    added serial number @ UnknownInputException

--- 12_CDIO_del2/src/exceptions/UnknownInputException.java
+++ 12_CDIO_del2/src/exceptions/UnknownInputException.java
@@ -2,6 +2,8 @@ package exceptions;
 
 public class UnknownInputException extends Exception{
 
+	private static final long serialVersionUID = 1L;
+
 	public UnknownInputException(){
 		super("Not a valid input");
 	}
